d111b65bf9091242798b43908bad9c98c48cfd74
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-05-19 15:01:25 +0100

    compiling network locally

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Arigato Smart Contract!");
+    }
+}
